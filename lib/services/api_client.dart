7b48739cdd767c0817306b0109c30ac051109212
sagarpuniynai <sagarpuniyani@gmail.com>
2023-12-12 20:05:52 +0530

    Changes for ApiCLient

--- lib/services/api_client.dart
+++ lib/services/api_client.dart
@@ -1,11 +1,12 @@
 import 'package:dio/dio.dart';
 
-class ApiClient{
-  Dio _dio = Dio();
+class ApiClient {
+  final Dio _dio = Dio();
 
-  getVideo(){
-    const URL = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics%2Cplayer&chart=mostPopular&maxResults=100&regionCode=In&key=AIzaSyA6hI_egrpWFQgL231-r9Z7kBoSut40xoc';
+  Future<dynamic> getVideo() async{
+    const url = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics%2Cplayer&chart=mostPopular&maxResults=100&regionCode=In&key=AIzaSyA6hI_egrpWFQgL231-r9Z7kBoSut40xoc';
 
-    _dio.get(URL);
+  final Response response = await  _dio.get(url);
+  return response.data ;
   }
 }
